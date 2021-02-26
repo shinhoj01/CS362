535abe0d2673f4f31fc700dcbc84792665ae8278
Hojun Shin <shinhoj@oregonstate.edu>
2021-02-25 22:21:00 -0800

    unittest done

--- In-class-activity-3/fibonacci.py
+++ In-class-activity-3/fibonacci.py
@@ -1,6 +1,6 @@
 
-def fibonacci(num):
-    if num <= 1:
-        return num
+def fibonacci(n):
+    if n <= 1:
+        return n
     else:
         return(fibonacci(n-1)+fibonacci(n-2))
--- In-class-activity-3/test_fibonacci.py
+++ In-class-activity-3/test_fibonacci.py
@@ -2,8 +2,10 @@ import unittest
 import fibonacci
 
 class TestCase(unittest.TestCase):
-    def test_fibonnaci(self):
+    def test_inputTwo(self):
         self.assertEqual(fibonacci.fibonacci(2), 1)
+    def test_inputTen(self):
+        self.assertEqual(fibonacci.fibonacci(10), 55)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
