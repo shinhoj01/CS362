055dab1a2e28f92ed8d30d91b7d2155ae009c22e
Hojun Shin <shinhoj@oregonstate.edu>
2021-03-03 01:54:48 -0800

    Test to Question 3
    Used map() to apply future function to list of inputs and compared with list of expected outputs

---
+++ HW7/test_LeapYear.py
@@ -0,0 +1,12 @@
+import pytest
+import code
+
+class TestCase:
+    def test(self):
+        # lists of inputs and outputs
+        inp = (2000, 2004, 1900, 2003)
+        otp = ["2000 is a leap year", "2004 is a leap year",
+               "1900 is not a leap year", "2003 is not a leap year"]
+        # Used map to apply the function to list
+        res = list(map(code, inp))
+        assert res == otp
